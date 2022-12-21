9c3eec9bf87d3fe14c42cc9e60caf2a119d75add
Anastasiya <9399023@mail.ru>
2022-12-22 00:31:34 +0300

    add task43

---
+++ task43/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+float b1 = ReadInt("Введите число b1: ");
+float k1 = ReadInt("Введите число k1: ");
+float b2 = ReadInt("Введите число b2: ");
+float k2 = ReadInt("Введите число k2: ");
+float x, y;
+
+if(k1==k2 && b1==b2)
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else if(k1==k2)
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else
+{
+    x = (b2-b1)/(k1-k2);
+    y = (k1 * (b2-b1)) / (k1-k2)+b1;
+    Console.WriteLine($"Пересечение в точке: ({x};{y})");
+}
+    // Console.WriteLine($"b1={b1}, k1={k1}, b2={b2}, k2={k2}");
+// double x = (b2-b1)/(k1-k2);
+// Console.WriteLine(x);
+// double y = k1 * x + b1;
+
+// if(y = k1 * x + b1 || y = k2 * x + b2)
+// {
+//     Console.WriteLine($"Пересечение в точке: ({x};{y})");
+// }
