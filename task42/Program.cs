bd03734f56e9f73f0e7dea6209aa583045cb4405
Anastasiya <9399023@mail.ru>
2022-12-21 22:44:14 +0300

    add task42

---
+++ task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = ReadInt("Введите число: ");
+
+PrintBinaryView(number);
+
+void PrintBinaryView(int n)
+{
+    if (n <=0) return;
+    PrintBinaryView(n /2);
+    Console.Write(n % 2);
+}
